be523fe4bc79c3d79b68032c7a900a3d4184d046
박수진 <wav0120@gmail.com>
2023-12-28 00:44:07 +0900

    Remane: Report -> Diary로 변경

---
+++ front/src/components/DiaryPage.js
@@ -0,0 +1,8 @@
+import React, { useContext, useState, useEffect } from "react";
+import DiaryForm from "./diary/DiaryForm";
+
+function DiaryPage() {
+  return <DiaryForm />;
+}
+
+export default DiaryPage;
--- front/src/components/diary/DiaryForm.js
+++ front/src/components/diary/DiaryForm.js
@@ -1,20 +1,22 @@
 import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { Container, Col, Row, Form, Button } from "react-bootstrap";
+import * as Api from "../../api";
 
-import * as Api from "../../Api";
-
-function ReportForm() {
+function DiaryForm() {
   const [username, setUsername] = useState("기본 닉네임");
   const [score, setScore] = useState([5, 5, 5]);
 
   const handleSubmit = async (e) => {
+    const userId = "01HJKR4QXTRA8E4KXFJWGCEAS8";
     e.preventDefault();
     console.log("요청 시도");
     try {
       // "user" 엔드포인트로 post요청
-      const res = await Api.post("user/score", {
-        score,
+      const res = await Api.post(`${userId}/diary`, {
+        meal: score[0],
+        sleep: score[1],
+        satisfaction: score[1],
       });
       console.log("응답: ", res);
     } catch (err) {
@@ -38,4 +40,4 @@ function ReportForm() {
   );
 }
 
-export default ReportForm;
+export default DiaryForm;
