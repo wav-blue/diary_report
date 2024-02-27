1d51603f6e2c3b68057e41862e51d602cb03eccf
박수진 <wav0120@gmail.com>
2024-02-27 22:50:56 +0900

    Diary module 설정, package.json 추가

--- back/src/diary/diary.module.ts
+++ back/src/diary/diary.module.ts
@@ -1,4 +1,22 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { LoggerModule } from 'src/logger/logger.module';
+import { DiaryController } from './diary.controller';
+import { DiaryCreateService } from './service/diaryCreate.service';
+import { DiaryReadService } from './service/diaryRead.service';
+import { IDiaryRepository } from './repository/DAO/diary.dao';
+import { DiaryRepository } from './repository/DAO/diary.repository';
 
-@Module({})
+@Module({
+  imports: [
+    LoggerModule,
+    JwtModule.register({ secret: 'hard!to-guess_secret' }),
+  ],
+  controllers: [DiaryController],
+  providers: [
+    DiaryCreateService,
+    DiaryReadService,
+    { provide: IDiaryRepository, useClass: DiaryRepository },
+  ],
+})
 export class DiaryModule {}
